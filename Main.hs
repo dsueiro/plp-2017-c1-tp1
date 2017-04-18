af4bf5228eff98d030dbbbbd5e55e5b7d552d018
pandaconqueso <malenaivnisky@gmail.com>
2017-04-18 02:04:54 -0300

    agrego mas tests...

--- Main.hs
+++ Main.hs
@@ -52,15 +52,30 @@ allTests = test [
 
 testsEj2 = test [
   0 ~=? capacidad soloUnMotor,
-  3 ~=? capacidad puroContenedor
+  3 ~=? capacidad puroContenedor,
+  3 ~=? capacidad nave9,
+  1 ~=? capacidad contenedorSolo,
+  2 ~=? capacidad superProtegido
   ]
 
 testsEj3 = test [
-  0 ~=? 0 --Cambiar esto por tests verdaderos.
+  contenedorSolo ~=? mayorCapacidad [contenedorSolo, soloUnMotor],
+  nave8 ~=? mayorCapacidad [nave6, nave8, contenedorSolo, soloUnMotor],
+  puroContenedor ~=? mayorCapacidad [puroContenedor, contenedorSolo, otroCañon]
   ]
 
 testsEj4 = test [
-  0 ~=? 0 --Cambiar esto por tests verdaderos.
+  soloUnMotor ~=? transformar (const Motor) soloUnMotor,
+  contenedorSolo ~=? transformar (const Contenedor) soloUnMotor,
+  nave3 ~=? transformar (\c -> if c == Motor then Cañón else (if c == Cañón then Motor else c)) nave2,
+  Módulo Contenedor 
+    (Módulo Contenedor (Módulo Contenedor (Base Contenedor) (Base Cañón)) (Módulo Motor (Base Contenedor) (Base Motor))) 
+    (Módulo Contenedor (Módulo Contenedor (Base Motor) (Base Contenedor)) (Módulo Contenedor (Base Cañón) (Base Contenedor)))
+    ~=? transformar (\c -> if c == Escudo then Contenedor else c) nave9,
+  Módulo Contenedor 
+    (Módulo Contenedor (Módulo Contenedor (Base Contenedor) (Base Contenedor)) (Módulo Contenedor (Base Contenedor) (Base Contenedor))) 
+    (Módulo Contenedor (Módulo Contenedor (Base Contenedor) (Base Contenedor)) (Módulo Contenedor (Base Contenedor) (Base Contenedor)))
+    ~=? transformar (const Contenedor) nave9
   ]
 
 testsEj5 = test [
@@ -68,20 +83,43 @@ testsEj5 = test [
   Módulo Cañón (Base Escudo) (Base Contenedor) ~=? impactar (Estribor, 1, Pequeño) nave2,
   Base Contenedor ~=? impactar (Babor, 0, Pequeño) nave1,
   nave1 ~=? impactar (Babor, 1, Pequeño) nave1,
-  Módulo Contenedor (Base Contenedor) (Módulo Motor (Base Escudo) (Base Cañón)) ~=? impactar (Babor, 1, Pequeño) nave4
+  Módulo Contenedor (Base Contenedor) (Módulo Motor (Base Escudo) (Base Cañón)) ~=? impactar (Babor, 1, Pequeño) nave4,
+  protegido ~=? impactar (Estribor, 0, Grande) protegido,
+  protegido ~=? impactar (Babor, 0, Pequeño) protegido,
+  contenedorSolo ~=? impactar (Babor, 0, Torpedo) protegido,
+  protegidoNivel1Estribor ~=? impactar (Estribor, 1, Grande) protegidoNivel1Estribor,
+  Módulo Contenedor (Base Motor) (Base Contenedor) ~=? impactar (Estribor, 1, Torpedo) protegidoNivel1Estribor,
+  contenedorSolo ~=? impactar (Babor, 0, Grande) escudoSinCañon,
+  escudoSinCañon ~=? impactar (Babor, 0, Pequeño) escudoSinCañon
   ]
 
 testsEj6 = test [
-  Módulo Contenedor (Base Contenedor) (Módulo Motor (Base Escudo) (Base Contenedor)) ~=? maniobrar nave4 [(Babor, 2, Pequeño), (Babor, 1, Pequeño), (Estribor, 2, Grande)]
+  Módulo Contenedor (Base Contenedor) (Módulo Motor (Base Escudo) (Base Contenedor)) ~=? maniobrar nave4 [(Babor, 2, Pequeño), (Babor, 1, Pequeño), (Estribor, 2, Grande)],
+  superProtegido ~=? maniobrar superProtegido [(Babor, 1, Grande), (Estribor, 1, Grande), (Babor, 1, Pequeño), (Estribor, 1, Pequeño)],
+  Módulo Motor contenedorSolo protegido ~=? maniobrar superProtegido [(Babor, 1, Grande), (Babor, 1, Torpedo)],
+  Módulo Motor contenedorSolo contenedorSolo ~=? maniobrar superProtegido [(Babor, 1, Torpedo), (Estribor, 1, Torpedo)]
   ]
 
 testsEj7 = test [
-  
+  [nave9, Módulo Escudo contenedorSolo (Módulo Escudo (Base Motor) (Base Cañón))] ~=? pruebaDeFuego [(Babor, 1, Grande), (Estribor, 1, Grande)] [nave9, protegidoNivel1Estribor, desbalanceado, Módulo Escudo contenedorSolo (Módulo Escudo (Base Motor) (Base Cañón))],
   3 ~=? length (pruebaDeFuego [(Babor,1,Grande),(Babor,2,Torpedo),(Estribor, 1, Pequeño)] [nave1,nave2,nave3,nave4,nave5,nave6,nave7,nave8,nave9])
   ]
 
 testsEj8 = test [
-  (4,6) ~=? (dimensiones $ maniobrar nave9 [(Babor,1,Grande),(Babor,2,Torpedo)])
+  1 ~=? componentesPorNivel nave9 0,
+  2 ~=? componentesPorNivel nave9 1, 
+  4 ~=? componentesPorNivel nave9 2,
+  8 ~=? componentesPorNivel nave9 3,
+  0 ~=? componentesPorNivel nave9 4,
+  2 ~=? componentesPorNivel desbalanceado 1,
+  2 ~=? componentesPorNivel desbalanceado 2,
+  (4,6) ~=? (dimensiones $ maniobrar nave9 [(Babor,1,Grande),(Babor,2,Torpedo)]),
+  (1,1) ~=? dimensiones contenedorSolo,
+  (2,2) ~=? dimensiones nave2,
+  (3,4) ~=? dimensiones nave4,
+  (4,4) ~=? dimensiones nave6,
+  (4,8) ~=? dimensiones nave9,
+  (3,2) ~=? dimensiones desbalanceado
   ]
 
 
